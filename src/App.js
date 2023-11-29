808b02c85b876debc6ef3aabb3eaa650869fd49e
Gowri Prashanth Kanagaraj <gw351441@dal.ca>
2023-11-29 15:31:57 -0400

    new frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Sentiment from './Sentiment';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Sentiment/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sentiment.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Sentiment() {
+    const [data, setData] = useState({ ExtractedText: '', Sentiment: '' });
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+  
+    useEffect(() => {
+        axios.get('https://42v84a67m5.execute-api.us-east-1.amazonaws.com/prod/polarity')
+          .then(response => {
+            setData(JSON.parse(response.data.body)); // Parse the JSON string
+            setIsLoading(false);
+          })
+          .catch(error => {
+            console.error('Error fetching data:', error);
+            setError(error);
+            setIsLoading(false);
+          });
+      }, []);
+  
+  
+    return (
+      <div>
+        <h1>Extracted Text and Sentiment</h1>
+        <p><strong>Extracted Text:</strong> {data.ExtractedText}</p>
+        <p><strong>Sentiment:</strong> {data.Sentiment}</p>
+      </div>
+    );
+}
+
+export default Sentiment;
